fb3551b5ce77c0e2892bc94c711486aa37c075a6
Fahime Zivdar <F.zivdar@nizek.com>
2024-03-30 09:59:46 +0330

    feat: 'core::domain' SearchUseCase added.

---
+++ core/domain/src/main/java/payback/group/domain/usecases/SearchUseCase.kt
@@ -0,0 +1,27 @@
+package payback.group.domain.usecases
+
+import kotlinx.coroutines.withContext
+import payback.group.data.remote.repository.SearchRepository
+import payback.group.model.Search
+import payback.group.shared.DispatcherProvider
+import payback.group.shared.FResult
+import payback.group.shared.kotlin.suspendSafeDataResult
+
+interface SearchImageUseCase {
+    suspend operator fun invoke(term: String): FResult<Search>
+}
+
+internal class SearchImageUseCaseImpl(
+    private val searchRepository: SearchRepository,
+    private val dispatcherProvider: DispatcherProvider
+) : SearchImageUseCase{
+
+    override suspend fun invoke(term: String): FResult<Search> {
+        return withContext(dispatcherProvider.io) {
+            suspendSafeDataResult {
+                searchRepository.search(term=term)
+            }
+        }
+    }
+
+}
