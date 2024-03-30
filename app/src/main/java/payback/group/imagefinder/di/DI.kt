928cd70fa34a1600c91214a1ca776e604aae0e91
Fahime Zivdar <F.zivdar@nizek.com>
2024-03-30 10:18:48 +0330

    feat(app): DI added.

---
+++ app/src/main/java/payback/group/imagefinder/di/DI.kt
@@ -0,0 +1,19 @@
+package payback.group.imagefinder.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import payback.group.domain.di.domainDiModule
+import payback.group.imagefinder.ui.screen.detail.DetailViewModel
+import payback.group.imagefinder.ui.screen.search.SearchViewModel
+
+val appDiModule = module {
+    includes(domainDiModule)
+
+    viewModel{
+        SearchViewModel(searchImageUseCase = get(), dispatcherProvider = get())
+    }
+
+    viewModel{parameters->
+        DetailViewModel(itemId = parameters.get(), detailUseCaseImpl = get(), dispatcherProvider = get())
+    }
+}
