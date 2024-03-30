658984dcbd03dec563704ee83b2b2e57652cb6f6
Fahime Zivdar <F.zivdar@nizek.com>
2024-03-30 10:05:26 +0330

    feat: 'core::data' SearchRepository added.

---
+++ core/data/src/main/java/payback/group/data/remote/repository/SearchRepository.kt
@@ -0,0 +1,27 @@
+package payback.group.data.remote.repository
+
+import payback.group.core.network.safeApiCall
+import payback.group.core.network.service.SearchService
+import payback.group.data.remote.mapper.responseToModel.map
+import payback.group.model.Search
+import payback.group.shared.FResult
+import payback.group.shared.mapIfSuccess
+
+interface SearchRepository {
+    suspend fun search(term: String): FResult<Search>
+    suspend fun detail(id: String): FResult<Search.Hit>
+}
+
+internal class SearchRepositoryImpl(
+    private val searchService: SearchService,
+) : SearchRepository {
+
+    override suspend fun search(term: String): FResult<Search> {
+        return safeApiCall { searchService.search(term = term) }.mapIfSuccess { it.map() }
+    }
+
+    override suspend fun detail(id: String): FResult<Search.Hit> {
+        return safeApiCall { searchService.search(id = id) }.mapIfSuccess { it.map().hits.first() }
+    }
+
+}
