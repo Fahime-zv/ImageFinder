c5cdab2a5f27d6d55678118176d1d7a1f94b4f0e
Fahime Zivdar <F.zivdar@nizek.com>
2024-03-31 11:12:29 +0330

    fix(app): 'Detail Component and Screen' changed

--- app/src/main/java/payback/group/imagefinder/ui/screen/detail/DetailComponent.kt
+++ app/src/main/java/payback/group/imagefinder/ui/screen/detail/DetailComponent.kt
@@ -1,36 +1,99 @@
 package payback.group.imagefinder.ui.screen.detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import payback.group.imagefinder.R
+import payback.group.imagefinder.ui.component.BeautyTextComponent
 import payback.group.imagefinder.ui.component.HeadingTextComponent
+import payback.group.imagefinder.ui.component.NormalTextComponent
+import payback.group.imagefinder.ui.theme.MainColor
 import payback.group.model.Search
 
 @Composable
 fun DetailComponent(hit: Search.Hit) {
     Column(
-        modifier = Modifier
-            .padding(16.dp)
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         AsyncImage(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxHeight(0.5f).fillMaxWidth()
+                .clip(RoundedCornerShape(0, 0, 10, 10)),
             model = hit.largeImageURL,
             contentDescription = "",
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
+            placeholder =painterResource(id = R.drawable.ic_launcher_foreground)
         )
 
-//        Spacer(modifier = Modifier.size(20.dp))
+        HeadingTextComponent(
+            value = "UserName: ${hit.user}", modifier = Modifier
+                .wrapContentSize()
+                .padding(20.dp,20.dp,20.dp,0.dp)
+        )
 
-        HeadingTextComponent(value ="UserName: ${hit.user}")
+        BeautyTextComponent(
+            value = "Tags: ** ${hit.tags} **",
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(20.dp)
+        )
+        NormalTextComponent(value = "Likes:${hit.likes}")
+        Spacer(modifier = Modifier.size(20.dp))
+        NormalTextComponent(value = "Downloads:${hit.downloads}")
+        Spacer(modifier = Modifier.size(20.dp))
+        NormalTextComponent(value = "Comments:${hit.comments}")
 
-//        Spacer(modifier = Modifier.size(10.dp))
-//
-//        AuthorDetailsComponent("Tags", hit.tags)
+    }
+}
 
+@Composable
+fun MyTopAppBar(onBackClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MainColor),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = { onBackClick.invoke() }
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.ArrowBack,
+                contentDescription = "Back",
+                tint = Color.Black
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = "Detail Page", color = Color.Black)
     }
 }
--- app/src/main/java/payback/group/imagefinder/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/payback/group/imagefinder/ui/screen/detail/DetailScreen.kt
@@ -1,19 +1,31 @@
 package payback.group.imagefinder.ui.screen.detail
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.koin.androidx.compose.getViewModel
 import org.koin.core.parameter.parametersOf
 import payback.group.imagefinder.architecture.ViewState
 import payback.group.imagefinder.ui.component.HeadingTextComponent
 import payback.group.imagefinder.ui.component.Loader
+import payback.group.imagefinder.ui.theme.ErrorOccured
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun DetailScreen( navController: NavController,itemId:String?) {
+fun DetailScreen(navController: NavController, itemId: String?) {
 
     val viewModel = getViewModel<DetailViewModel>(
         parameters = { parametersOf(itemId) }
@@ -21,30 +33,38 @@ fun DetailScreen( navController: NavController,itemId:String?) {
 
     val state by viewModel.viewStateFlow.collectAsState()
 
+    Scaffold(
+        topBar = { MyTopAppBar(onBackClick = { navController.navigateUp() }) }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
 
-    when (state) {
-        is ViewState.Loading -> {
-            Log.d(ContentValues.TAG, "Loading ")
-            Loader()
-        }
+            when (state) {
+                is ViewState.Loading -> {
+                    Log.d(ContentValues.TAG, "Loading ")
+                    Loader()
+                }
 
-        is ViewState.Success -> {
-            val res = (state as ViewState.Success).model
-            Log.d(ContentValues.TAG, "Success${res} ")
-            DetailComponent(res)
-        }
+                is ViewState.Success -> {
+                    val res = (state as ViewState.Success).model
+                    Log.d(ContentValues.TAG, "Success $res ")
+                    DetailComponent(res)
+                }
 
-        is ViewState.Error -> {
-            val res = (state as ViewState.Error)
-            Log.d(ContentValues.TAG, "Error ${res}")
+                is ViewState.Error -> {
+                    val res = (state as ViewState.Error)
+                    Log.d(ContentValues.TAG, "Error $res")
+                    HeadingTextComponent(value = ErrorOccured)
+                }
 
-        }
-
-        is ViewState.Empty ->{
-            //Nothing
+                is ViewState.Empty -> {
+                    //Nothing
+                }
+            }
         }
     }
-
-
 }
 
